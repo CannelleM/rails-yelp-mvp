21b133161227f902dc4a885494d98dadfa86d269
Cannelle Magdane <cannelle.magdane@gmail.com>
2022-11-16 11:39:25 +0100

    added validations to restaurant and review models

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  validates :rating, :content, presence: true
+  validates :rating, numericality: { only_integer: true }, inclusion: { in: [0, 1, 2, 3, 4, 5], message: 'Not a valid rating (must be between 0 and 5)' }
 end
